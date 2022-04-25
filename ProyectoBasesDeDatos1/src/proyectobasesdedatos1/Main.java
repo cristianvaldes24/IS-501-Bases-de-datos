46c90eec7f4c6dc87517fbe2aa3b471254474183
Eber Josué Domínguez Ramírez <45218164+jossdoming@users.noreply.github.com>
2022-04-24 18:39:42 -0600

    Add files via upload

---
+++ ProyectoBasesDeDatos1/src/proyectobasesdedatos1/Main.java
@@ -0,0 +1,155 @@
+
+package proyectobasesdedatos1;
+import org.mariadb.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Main extends javax.swing.JFrame {
+Connection con = null;      
+String sURL = "jdbc:mariadb://proyecto.co7ggp7wsw2h.us-east-1.rds.amazonaws.com:3306";
+Connection coneccion = (Connection) DriverManager.getConnection(sURL,"admin","administrador");
+ 
+    public Main() throws SQLException {
+        initComponents();
+        
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Modern No. 20", 0, 36)); // NOI18N
+        jLabel1.setText("PROYECTO TALLER AUTOMOTRIZ");
+
+        jButton1.setText("REGISTRAR USUARIO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("CONSULTAR USUARIO");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("BORRAR USUARIO");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(79, 79, 79)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(97, 97, 97))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 591, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(213, 213, 213)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(55, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
+           
+String URL = "jdbc:mariadb://proyecto.co7ggp7wsw2h.us-east-1.rds.amazonaws.com:3306";
+Connection cxon = (Connection) DriverManager.getConnection(URL,"admin","administrador");
+  try (PreparedStatement db = cxon.prepareStatement("USE Proyecto;")) {
+    // Ejecutamos Query
+    ResultSet dt = db.executeQuery();}
+  try (PreparedStatement stmt = cxon.prepareStatement("select nombre from empleado")) {
+    // Ejecutamos Query
+    ResultSet rs = stmt.executeQuery();
+     
+    // Recorremos el resultado
+    while (rs.next())
+      System.out.println (rs.getString("nombre"));
+    } catch (SQLException sqle) { 
+      System.out.println("Error en la ejecución:" 
+    + sqle.getErrorCode() + " " + sqle.getMessage());    
+    }
+} catch (SQLException e) { 
+  System.out.println("Error en la conexión:" + e.toString() );
+} finally {
+  try {
+    // Cerramos posibles conexiones abiertas
+    if (con!=null) con.close();    
+  } catch (SQLException e) {
+    System.out.println("Error cerrando conexiones: "
+      + e.toString());
+  } 
+}
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    
+    public static void main(String args[]) throws SQLException {
+        
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new Main().setVisible(true);
+                } catch (SQLException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ ProyectoBasesDeDatos1/src/proyectobasesdedatos1/ProyectoBasesDeDatos1.java
@@ -0,0 +1,15 @@
+
+package proyectobasesdedatos1;
+
+import java.sql.SQLException;
+
+
+public class ProyectoBasesDeDatos1 {
+
+
+    public static void main(String[] args) throws SQLException {
+        Main mostrar = new Main();
+        mostrar.setVisible(true);
+    }
+    
+}
